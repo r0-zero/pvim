33a21bcb1ba961c9c712fb68aa67939ede732ef1
Arda Atci <phystecharda@gmail.com>
2026-02-27 22:31:15 +0300

    lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,6 +2,5 @@ require("nvchad.configs.lspconfig").defaults()
 
 local servers = { "html", "cssls", "clangd" }
 vim.lsp.enable(servers)
-vim.lsp.enable("clangd")
 
 -- read :h vim.lsp.config for changing options of lsp servers
